d8955ea421e84a607902b34888840c073525fe6f
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-08-18 14:07:30 -0700

    added a sample route

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ var port = process.env.PORT || 5000;
 var app = server.listen(port, function() {
   console.log('server listening on port ' + port);
 });
+
+server.get('/sample', function (req, res) {
+  console.log('GET /sample called.');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.status(200).send('Hello This is a sample response');
+});
