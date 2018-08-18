08c1148e80a05eaa5dcb27684a83e38dd026c28f
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-08-18 16:00:43 -0700

    home page

--- www/js/app.js
+++ www/js/app.js
@@ -3,5 +3,9 @@ var app = angular.module('root', ['ngMaterial']);
 app.controller('index', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
     $scope.title = 'City of San Jose: Parks';
-    
+
+
+
+    $scope.sampleEvents = [{}, {}];
+
 }]);
