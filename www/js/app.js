ebd5d2529b0e01f5cf55722da979e949afacf48e
Khantil Choksi <khchoksi@ncsu.edu>
2018-08-18 18:46:06 -0700

    categories

--- www/js/app.js
+++ www/js/app.js
@@ -8,4 +8,23 @@ app.controller('index', ['$scope', '$http', '$window', function ($scope, $http, 
 
     $scope.sampleEvents = [{}, {}];
 
+
+    // Categories
+    $scope.items = ["Sports", "Dance", "Art"];
+      $scope.selected = [];
+
+      $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+          list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+      };
+
+      $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+      };
+
 }]);
