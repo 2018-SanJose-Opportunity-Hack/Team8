f654c7a748d4b4a86d1fef5e8e9fab1d272ab188
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-08-18 18:40:20 -0700

    added geolocation stuff

--- www/js/app.js
+++ www/js/app.js
@@ -12,19 +12,45 @@ app.controller('index', ['$scope', '$http', '$window', function ($scope, $http, 
         console.log("Error getting data from the communities JSON file.");
     });
 
+    $scope.getLocation = function () {
+        console.log('fetching location.');
+        if (navigator.geolocation) {
+            console.log('geolocation available.');
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $scope.$apply(function () {
+                    $scope.position = { 'latitude': position.coords.latitude, 'longitude': position.coords.longitude };
+                    console.log('Dummy distance: ' + calculatedistance(position.coords.latitude, position.coords.latitude + 10, position.coords.longitude, position.coords.longitude + 10) + ' meters');
+                });
+            });
+        } else {
+            console.log('no geolocation');
+            $scope.$apply(function () {
+                $scope.position = 'no geolocation available!';
+            });
+        }
+    }
+
+    if (Number.prototype.toRadians === undefined) {
+        Number.prototype.toRadians = function () { return this * Math.PI / 180; };
+    }
+
+    calculatedistance = function (lat1, lat2, lon1, lon2) {
+        var φ1 = lat1.toRadians(), φ2 = lat2.toRadians(), Δλ = (lon2 - lon1).toRadians(), R = 6371e3; // gives d in metres
+        var d = Math.acos(Math.sin(φ1) * Math.sin(φ2) + Math.cos(φ1) * Math.cos(φ2) * Math.cos(Δλ)) * R;
+        return d;
+    }
+
     $scope.communityEvents = [];
 
     $scope.communitySelected = function (selectedCommunity) {
         console.log('Comunity selected: ' + selectedCommunity);
         loadCommunityEvents(selectedCommunity);
-        // call api or something to fetch the events of this community.
-        // and then, set the $scope.
     };
 
     loadCommunityEvents = function (selectedCommunity) {
         console.log('Loading the community Events');
         $http.get('community-events?community=' + selectedCommunity).then(function (data) {
-            console.log('DATA: ' + JSON.stringify(data));
+            // console.log('DATA: ' + JSON.stringify(data));
             $scope.communityEvents = data.data;
         }, function (err) {
             console.log("Error getting data from the communities JSON file.");
